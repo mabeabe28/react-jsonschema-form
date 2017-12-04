f49b6008e687f7bc19e572b6d6f019493004237f
Marko Sustarsic <MarkoS@canneslions.com>
2017-12-04 16:19:46 +0000

    Build

--- test/setup-jsdom.js
+++ test/setup-jsdom.js
@@ -3,7 +3,9 @@ var jsdom = require("jsdom");
 // Setup the jsdom environment
 // @see https://github.com/facebook/react/issues/5046
 if (!global.hasOwnProperty("window")) {
-  global.document = (new jsdom.JSDOM("<!doctype html><html><body></body></html>")).window.document;
+  global.document = new jsdom.JSDOM(
+    "<!doctype html><html><body></body></html>"
+  ).window.document;
   global.window = document.defaultView;
   global.navigator = global.window.navigator;
 }
